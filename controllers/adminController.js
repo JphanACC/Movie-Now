88cf2a734494aafc18c6bf4116e679ca82995561
jphan <jphanacc@gmail.com>
2020-09-20 15:35:37 -0700

    working-in-progress: basic element layout for main homepage

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -9,11 +9,11 @@ router.get("/", (req, res) => {
 });
 
 router.get("/newMovie", (req, res) => {
-    res.render("admin/newMovie", { title: "Make a New Movie" });
+    res.render("admin/newMovie", { title: "Make a New Movie", css: "main" });
 });
 
 router.get("/newTheatre", (req, res) => {
-    res.render("admin/newTheatre", { title: "Make a New Theatre" });
+    res.render("admin/newTheatre", { title: "Make a New Theatre", css: "main" });
 });
 
 module.exports = router;
