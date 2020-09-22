9eb25218c82a1d9b261f3ce06b9a53059980169b
JeffreyThompson <jeffathomp@gmail.com>
2020-09-22 10:54:11 -0500

    removes arrays from movie model

--- models/Movie.js
+++ models/Movie.js
@@ -6,8 +6,8 @@ const movieSchema = new mongoose.Schema({
     genre: { type: String, required: [true, "You must provide a genre."] },
     yearReleased: { type: String, required: [true, "You must provide a release year."] },
     plotSummary: { type: String, required: [true, "You must provide a plot summary."] },
-    actors: { type: [String], required: [true, "You must provide at least one actor."] },
-    images: { type: [String], required: [true, "You must provide at least one image."] },
+    actors: { type: String, required: [true, "You must provide at least one actor."] },
+    images: { type: String, required: [true, "You must provide an one image."] },
     Theatres: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "Theatre",
