80b4b92dce06c8fb4ddd56ee28b412f9bbb4c55e
JeffreyThompson <jeffathomp@gmail.com>
2020-09-23 15:37:08 -0500

    edit showing update route in progress

--- controllers/showingController.js
+++ controllers/showingController.js
@@ -8,11 +8,8 @@ const db = require("../models");
 router.post("/", (req, res) => {
     console.log(req.body);
     for (const key in req.body.playing) {
-
         console.log(key);
-
         if (req.body.playing[key][0] === 'on') {
-
             const newShowing = {
                 time: key,
                 price: req.body.price,
@@ -20,17 +17,46 @@ router.post("/", (req, res) => {
                 Movie: req.body.playing[key][1],
                 playing: true,
             }
-            console.log("Debug 2:---------------- Check newShowing");
-            console.log(newShowing);
             db.Showing.create(newShowing, (err, createdShowing) => {
                 if (err) return res.send(err);
                 console.log("Debug 3");
-
-            })
+            });
         }
     }
     res.redirect("/admin/");
+});
 
+router.put("/", (req,res) => {
+    console.log(req.body);
+    for (const key in req.body) {
+        console.log(req.body[key]);
+        if (req.body[key][0] === 'on') {
+            const editShowing = {
+                price: req.body[key][2],
+                Movie: req.body[key][1],
+                playing: true,
+            }
+            db.Showing.findByIdAndUpdate(req.body[key], editShowing, {new: true}, (err, updatedShowing) => {
+                if (err) {
+                    console.log(err);
+                    res.send(err);
+                }
+                console.log(updatedShowing);
+            })
+        } else {
+            const editShowing = {
+                playing: false,
+            }
+            db.Showing.findByIdAndUpdate(req.body[key], editShowing, {new: true}, (err, updatedShowing) => {
+                if (err) {
+                    console.log(err);
+                    res.send(err);
+                }
+                console.log(updatedShowing);
+            })
+        }
+    }
+    res.redirect("/admin");
 })
 
 module.exports = router;
