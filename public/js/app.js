bc2e56ecca562824e46e9dcf160dcb3523631baa
JeffreyThompson <jeffathomp@gmail.com>
2020-09-24 14:44:41 -0500

    total price styling

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,3 @@
-// const theatreSelect = $('.option')
-
-// const changeTheatre = function(e) {
-//     const option = e.target.id;
-//     console.log(option);
-// }
-
-// theatreSelect.on("select", changeTheatre);
-console.log("app.js");
-
 const $ticketQty = $(".ticketQty");
 
 const updatePrice = function() {
@@ -15,7 +5,7 @@ const updatePrice = function() {
     const $adultQty = Number($("#adultQty").val());
     const $childQty = Number($("#childQty").val());
     const total = $adultQty+ $childQty;
-    $totalPrice.text(total); 
+    $totalPrice.text(`Total Price: $${total}`); 
 };
 
 $ticketQty.on("change", updatePrice);
